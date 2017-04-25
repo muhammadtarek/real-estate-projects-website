de6e30b0116ea3e6c7357a5e9b4e06856bc4f0f1
Muhammad Tarek <muhammadtarek96@gmail.com>
2017-04-25 22:18:42 +0200

    Moving layouts to shared

--- Project Apollo/Controllers/welcomeController.cs
+++ Project Apollo/Controllers/welcomeController.cs
@@ -11,6 +11,7 @@ namespace Project_Apollo.Controllers
         // GET: welcome
         public ActionResult Index()
         {
+			ViewBag.currentPage = 0;
             return View();
         }
     }
