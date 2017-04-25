30c3cdde1c28479df7347221294a71e89b71732e
mohamedkamal274 <muhammedkmal3@gmail.com>
2017-04-25 20:15:37 +0200

    update & delete project

--- Project Apollo/Controllers/homeController.cs
+++ Project Apollo/Controllers/homeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_Apollo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,12 +9,41 @@ namespace Project_Apollo.Controllers
 {
     public class HomeController : Controller
     {
+        DBase db = new DBase();
         // GET: Home
         public ActionResult Index()
         {
             ViewBag.showNav = true;
             ViewBag.tabs = new string[4] { "Home", "Profile", "FAQ", "Contact us" };
             return View();
+        }
+        public JsonResult ResultdeleteProject(int id)
+        {
+            Project p = db.ProjectTable.Find(id);
+
+            if (p != null)
+            {
+                db.ProjectTable.Remove(p);
+                db.SaveChanges();
+
+                return Json(new { opertaion = false });
+            }
+            return Json(new { opertaion = false });
+        }
+        [HttpPost]
+        public JsonResult updateProject(string projectName, string projectDescription, int projectId)
+        {
+            Project p = db.ProjectTable.Find(projectId);
+            p.Name = projectName;
+            p.Description = projectDescription;
+            db.SaveChanges();
+            return Json(new
+            {
+                postingTime = p.createDate,
+                projectName = p.Name,
+                projectDescription = p.Description,
+                projectId = p.ID
+            });
         }
     }
 }
