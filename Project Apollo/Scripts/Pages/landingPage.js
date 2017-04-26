5e3aed6f1741e786cac7d5cf18863323941dfc03
Muhammad Tarek <muhammadtarek96@gmail.com>
2017-04-26 23:11:02 +0200

    Finshing landing page and bugs squashing

--- Project Apollo/Scripts/Pages/landingPage.js
+++ Project Apollo/Scripts/Pages/landingPage.js
@@ -4,8 +4,8 @@
     var loginForm = "#login-form";
     var signUpForm = "#signup-form";
 
-    $(loginForm).css("margin-top", "-435px");
-    $(signUpForm).css("margin-top", "-639px");
+    $(loginForm).css("top", "-435px");
+    $(signUpForm).css("top", "-639px");
 
     /* FUNCTIONS */
     function showForm(formId) {
@@ -14,14 +14,14 @@
 
         $(form + ", .popup-container").css("display", "block");
         setTimeout(function () {
-            $(form).css("margin-top", "35px");
+            $(form).css("top", "35px");
 
         }, 2);
     }
 
     function hideForm() {
-        $(loginForm).css("margin-top", "-435px");
-        $(signUpForm).css("margin-top", "-639px");
+        $(loginForm).css("top", "-435px");
+        $(signUpForm).css("top", "-639px");
 
         setTimeout(function () {
             $(".form, .popup-container").css("display", "none");
@@ -40,4 +40,19 @@
     $(".close").click(function () {
         hideForm();
     })
+
+    //Taking the photo from file uploader and preview it
+    $("#signup-photo").change(function () {
+      var file = document.querySelector('input[type=file]').files[0];
+      var reader = new FileReader();
+      console.log("Triggered");
+
+      reader.addEventListener("load", function () {
+        $("#photo-preview").attr('src', reader.result);
+      }, false);
+
+      if (file) {
+        reader.readAsDataURL(file);
+      }
+    });
 });
