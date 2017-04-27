c1ebc0ee79b211fcf8e442523b9342af812c332f
Megz20140415 <mostafa_magdy@fci.helwan.edu.eg>
2017-04-27 09:46:50 +0200

    signUp is done
    function signUp is finished with operation on photo

--- Project Apollo/Controllers/welcomeController.cs
+++ Project Apollo/Controllers/welcomeController.cs
@@ -2,27 +2,28 @@
 using Project_Apollo.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 namespace Project_Apollo.Controllers
 {
-    
+
     public class welcomeController : Controller
     {
         public DBase db = new DBase();
         // GET: welcome
         public ActionResult Index()
         {
-			ViewBag.currentPage = 0;
+            ViewBag.currentPage = 0;
             return View();
         }
 
         public Object login(string email, string password)
-        {         
+        {
             var data = (from d in db.userTable
-                         where d.Email == email
-                         select new { d.ID, d.name, d.UserRole, d.Photo , d.Password}).ToArray();
+                        where d.Email == email
+                        select new { d.ID, d.name, d.UserRole, d.Photo, d.Password }).ToArray();
 
             if (data.Length != 0 && password.Equals(data[0].Password)) // if email & password TRUE
             {
@@ -41,8 +42,9 @@ namespace Project_Apollo.Controllers
                         userPhoto = data[0].Photo
                     }
                 });
-            }else if (data.Length != 0 && !password.Equals(data[0].Password)) // Email is true password is wrong
-                {
+            }
+            else if (data.Length != 0 && !password.Equals(data[0].Password)) // Email is true password is wrong
+            {
                 return JsonConvert.SerializeObject(new
                 {
                     Result = new
@@ -58,7 +60,8 @@ namespace Project_Apollo.Controllers
                         userPhoto = ""
                     }
                 });
-            }else if(data.Length == 0)  // email is wrong
+            }
+            else if (data.Length == 0)  // email is wrong
             {
                 return JsonConvert.SerializeObject(new
                 {
@@ -76,7 +79,83 @@ namespace Project_Apollo.Controllers
                     }
                 });
             }
-            return null;             
+            return null;
+        }
+
+        public object signUp(HttpPostedFileBase userPicture, string name, string email, string password, string phoneNumber, string Desciption, int userType = 1)
+        {
+
+            var v = (from a in db.userTable
+                     where a.Email == email
+                     select a.Email);
+
+            if (v.Count() > 0)
+            {
+                return JsonConvert.SerializeObject(new
+                {
+                    result = new
+                    {
+                        email = "Email is alrady registered"
+                    },
+                    user = new
+                    {
+                        id = "",
+                        name = "",
+                        userRole = "",
+                        userPhoto = ""
+                    }
+                });
             }
+            else
+            {
+                User user = new User();
+                user.name = name;
+                user.Email = email;
+                user.Password = password;
+                user.Mobile = phoneNumber;
+                user.UserRole = (userRole)userType;
+                user.Description = Desciption;
+
+                if (userPicture != null)
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        userPicture.InputStream.CopyTo(ms);
+                        byte[] arr = ms.GetBuffer();
+                        user.Photo = arr;
+                    }
+                }
+                db.userTable.Add(user);
+                db.SaveChanges();
+
+                var img = "";
+                if (user.Photo != null)
+                {
+                    var base64 = Convert.ToBase64String(user.Photo);
+                    img = String.Format("data:image/gif;base64,{0}", base64);
+                }
+
+                ViewBag.userPhoto = img;
+                ViewBag.userName = user.name;
+                ViewBag.userID = user.ID;
+                ViewBag.userRole = user.UserRole;
+
+
+                return JsonConvert.SerializeObject(new
+                {
+                    result = new
+                    {
+                        email = true
+                    },
+                    user = new
+                    {
+                        id = user.ID,
+                        name = user.name,
+                        userRole = userType,
+                        userPhoto = img
+                    }
+                });
+            }
+        }
     }
 }
