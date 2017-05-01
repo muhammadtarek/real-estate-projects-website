c7280c6e3ad256702074b9cccb1301c396ef5533
Muhammad Tarek <muhammadtarek96@gmail.com>
2017-05-01 17:52:44 +0200

    Adding two more steps validation before logging in or signing up

--- Project Apollo/Scripts/Pages/landingPage.js
+++ Project Apollo/Scripts/Pages/landingPage.js
@@ -1,4 +1,4 @@
-﻿$(window).load(function () {
+﻿$(window).ready(function () {
     var formHeight = 0;
 
     var loginForm = "#login-form";
@@ -6,6 +6,25 @@
 
     $(loginForm).css("top", "-435px");
     $(signUpForm).css("top", "-639px");
+
+    function showForm(formId) {
+        var form = "#" + formId;
+
+        $(form + ", .popup-container").css("display", "block");
+        setTimeout(function () {
+            $(form).css("top", "35px");
+
+        }, 2);
+    }
+
+    function hideForm() {
+        $(loginForm).css("top", "-435px");
+        $(signUpForm).css("top", "-639px");
+
+        setTimeout(function () {
+            $(".form, .popup-container").css("display", "none");
+        }, 260);
+    }
 
     /* ACTION LISENTERS */
     $("#login-btn").click(function () {
@@ -21,7 +40,7 @@
     })
 
     //Taking the photo from file uploader and preview it
-    $("#signup-photo").change(function () {
+    $("#user-photo").change(function () {
       var file = document.querySelector('input[type=file]').files[0];
       var reader = new FileReader();
       console.log("Triggered");
@@ -35,84 +54,100 @@
       }
     });
 
-    /*Click button ajax part*/
+    /*Action lisenters*/
     $("#btn-signup").click(signUp);
     $("#btn-login").click(login);
 });
 
 /* FUNCTIONS */
-
-function showForm(formId) {
-  var form = "#" + formId;
-
-  $(form + ", .popup-container").css("display", "block");
-  setTimeout(function () {
-    $(form).css("top", "35px");
-
-  }, 2);
-}
-
-function hideForm() {
-  $(loginForm).css("top", "-435px");
-  $(signUpForm).css("top", "-639px");
-
-  setTimeout(function () {
-    $(".form, .popup-container").css("display", "none");
-  }, 260);
-}
-
 function signUp() {
-    var email = getInputValue("signup-email");
-    var name = getInputValue("signup-name");
-    var phone = getInputValue("signup-phone");
-    var password = getInputValue("signup-password");
-    var userrole = getInputValue("signup-userrole");
-    var bio = getInputValue("signup-bio");
-    var filesSelected = $("#photo-preview").attr('src');
+    if (checkForEmptyFields("signup") && checkForDangerFields()) {
+        var email = getInputValue("signup-email");
+        var name = getInputValue("signup-name");
+        var phone = getInputValue("signup-phone");
+        var password = getInputValue("signup-password");
+        var userrole = getInputValue("signup-userrole");
+        var bio = getInputValue("signup-bio");
+        var filesSelected = $("#photo-preview").attr('src');
 
-    var datasend = {
-        userPicture: filesSelected,
-        name: name,
-        email: email,
-        password: password,
-        phoneNumber: phone,
-        Desciption: bio,
-        userType : userrole
+        var datasend = {
+            userPicture: filesSelected,
+            name: name,
+            email: email,
+            password: password,
+            phoneNumber: phone,
+            Desciption: bio,
+            userType: userrole
+        }
+        $.post("/welcome/signUp", datasend, function (data) {
+            rec = JSON.parse(data);
+            console.log(rec);
+            if (rec.result.email !== true) {
+                markInputAs("signup-email", DANGER, rec.result.email);
+            }
+            else {
+                markInputAs("signup-email", SUCCESS, "");
+                var url = "/home/Index?id=" + rec.user.id;
+                window.location.href = url;
+            }
+        });
     }
-    $.post("/welcome/signUp", datasend, function (data) {
-        rec = JSON.parse(data);
-        console.log(rec);
-        if (rec.result.email !== true) {
-            markInputAs("signup-email", DANGER, rec.result.email);
-        }
-        else {
-            markInputAs("signup-email", SUCCESS, "");
-            var url = "/home/Index?id=" + rec.user.id;
-            window.location.href = url;
-        }
-    });
 }
 
 function login() {
-    var email = getInputValue("login-email");
-    var password = getInputValue("login-password");
-    var datasend = {
-        email: email,
-        password: password,
+    if (checkForEmptyFields("login") && checkForDangerFields()) {
+        var email = getInputValue("login-email");
+        var password = getInputValue("login-password");
+        var datasend = {
+            email: email,
+            password: password,
+        }
+        $.post("/welcome/login", datasend, function (data) {
+            rec = JSON.parse(data);
+            if (rec.Result.Email !== true) {
+                markInputAs("login-email", DANGER, rec.Result.Email);
+            }
+            else if (rec.Result.password !== true) {
+                markInputAs("login-password", DANGER, rec.Result.password);
+            }
+            else {
+                markInputAs("login-email", SUCCESS);
+                markInputAs("login-password", SUCCESS);
+                var url = "/home/Index?id=" + rec.user.id;
+                window.location.href = url;
+            }
+        });
     }
-    $.post("/welcome/login", datasend, function (data) {
-        rec = JSON.parse(data);
-        if(rec.Result.Email !== true){
-            markInputAs("login-email", DANGER, rec.Result.Email);
-        }
-        else if (rec.Result.password !== true) {
-            markInputAs("login-password", DANGER, rec.Result.password);
-        }
-        else{
-            markInputAs("login-email", SUCCESS);
-            markInputAs("login-password", SUCCESS);
-            var url = "/home/Index?id=" + rec.user.id;
-            window.location.href = url;
-        }
-    });
+}
+
+function checkForEmptyFields(form) {
+    var inputs = $("input[id^='" + form + "-']")
+        .map(function () {
+            if ($(this).val().length === 0)
+                return $(this);
+        }).get();
+
+    //Returning true if all inputs have value
+    if (inputs.length == 0)
+        return true;
+
+    //Focusing on first empty field
+    var firstDangerInput = inputs[0][0].id;
+    markInputAs(firstDangerInput, DANGER, "Input is required");
+    $("#" + firstDangerInput).focus();
+}
+
+function checkForDangerFields() {
+    var inputs = $(".input-field--danger")
+        .map(function () {
+            return $(this); 
+        }).get();
+
+    //Returning true if all inputs are valid
+    if (inputs.length == 0)
+        return true;
+
+    //Focusing on first empty field
+    var firstDangerInput = inputs[0][0].id;
+    $("#" + firstDangerInput).focus();
 }
