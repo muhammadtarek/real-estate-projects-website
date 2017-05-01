5ce0ad4ec76a627d9a44c058cc5c93827c01b6bc
Muhammad Tarek <muhammadtarek96@gmail.com>
2017-05-01 18:54:11 +0200

    Adding delete project action

--- Project Apollo/Scripts/Pages/HomePage.js
+++ Project Apollo/Scripts/Pages/HomePage.js
@@ -1,1 +1,12 @@
-﻿
+﻿$(window).ready(function() {
+    var selectedPost;
+
+    $(".delete-btn").click(function () {
+        var projectContainer = $(this).parent().parent().parent().parent();
+        deleteProject($(projectContainer).attr("id"));
+    });
+});
+
+function deleteProject(projectId) {
+    $("#" + projectId).remove();
+}
