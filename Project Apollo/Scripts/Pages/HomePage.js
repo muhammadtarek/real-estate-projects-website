c8b33e519f03b465900af19b6b31a49428a75960
Muhammad Tarek <muhammadtarek96@gmail.com>
2017-05-01 20:51:09 +0200

    Connecting delete project

--- Project Apollo/Scripts/Pages/HomePage.js
+++ Project Apollo/Scripts/Pages/HomePage.js
@@ -2,26 +2,29 @@
     var selectedPostId;
 
     //Deleting a project post
-    $(".delete-btn").click(function () {
+    $(".delete-btn").click(function deleteProject() {
         var projectContainer = $(this).parent().parent().parent().parent();
-        deleteProject($(projectContainer).attr("id"));
+        var projectId = $(projectContainer).attr("id");
+
+        //Deleting from database
+        $.post("/home/deleteProject", { id: projectId }, function (operationResults) {
+            success = JSON.parse(operationResults);
+            if (success.opertaion)
+                $("#" + projectId).remove();
+        });
     });
 
     //Editing a project post
-    $(".edit-btn").click(function () {
+    $(".edit-btn").click(function editProject() {
         var projectContainer = $(this).parent().parent().parent().parent();
         selectedPostId = $(projectContainer).attr("id");
         $("#customer-form").attr("formAction", "edit");
 
         //Getting project details
-        $.post("/home/getProject", {projectId : selectedPostId}, function (projectData) {
+        $.post("/home/getProject", {projectId : selectedPostId }, function (projectData) {
             projectDetails = JSON.parse(projectData);
             $("#project-name").val(projectDetails.Name);
             $("#project-description").val(projectDetails.Description);
         });
     });
 });
-
-function deleteProject(projectId) {
-    $("#" + projectId).remove();
-}
