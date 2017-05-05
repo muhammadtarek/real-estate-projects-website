d650c8e139946d2bfc8b5ea22572781f07830353
Muhammad Tarek <muhammadtarek96@gmail.com>
2017-05-05 17:20:37 +0200

    Adding snackbar component

---
+++ Project Apollo/Scripts/Components/Snackbar.js
@@ -0,0 +1,18 @@
+﻿function showSnackbar(message) {
+    $(".snackbar label").text(message);
+    $(".snackbar").css("display", "block");
+
+    setTimeout(function () {
+        $(".snackbar").css("top", "86%");
+    }, 10);
+
+    setTimeout(function () {
+        hideSnackbar();
+    }, 7000);
+}
+
+function hideSnackbar() {
+    $(".snackbar").css("top", "110%");
+}
+
+$(".snackbar button").attr('onclick', 'hideSnackbar();');
