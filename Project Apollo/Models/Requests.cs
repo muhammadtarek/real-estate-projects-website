df5b52fbea44f49c28d8ab89624218e2ae0010f5
mohamedkamal274 <muhammedkmal3@gmail.com>
2017-05-11 17:29:25 +0200

    add request enum to request model

--- Project Apollo/Models/Requests.cs
+++ Project Apollo/Models/Requests.cs
@@ -6,12 +6,17 @@ using System.Web;
 
 namespace Project_Apollo.Models
 {
+    public enum request
+    {
+        teamLeaderRequest = 0,
+        juniorEngineerRequest = 1
+    }
     public class Requests
     {
         public int ID { get; set; }
         public virtual User sender { get; set; }
         public virtual User reciever { get; set; }
         public virtual Project project { get; set; }
-        public string requestType { get; set; }
+        public request requestType { get; set; }
     }
 }
