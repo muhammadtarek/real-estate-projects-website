b32cddbfcc0308c496a9a7f7aa01dd6e7221ded0
Muhammad Tarek <muhammadtarek96@gmail.com>
2017-05-16 01:00:49 +0200

    Moving form actions to a seperate file

---
+++ Project Apollo/Scripts/Actions/Form.js
@@ -0,0 +1,27 @@
+﻿$(window).ready(function () {
+    $("#login-form").css("top", "-435px");
+    $("#signup-form").css("top", "-739px");
+
+    $(".close").click(function () {
+        hideForm();
+    })
+});
+
+function showForm(formId) {
+    var form = "#" + formId;
+
+    $(form + ", .popup-container").css("display", "block");
+    setTimeout(function () {
+        $(form).css("top", "35px");
+
+    }, 2);
+}
+
+function hideForm() {
+    $("#signup-form").css("top", "-639px");
+    $("#login-form").css("top", "-435px");
+
+    setTimeout(function () {
+        $(".form, .popup-container").css("display", "none");
+    }, 260);
+}
