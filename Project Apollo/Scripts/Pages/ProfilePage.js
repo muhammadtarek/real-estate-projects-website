3d3803ddd35a3129ed25ac39967c685ed47861c7
mohamedkamal274 <muhammedkmal3@gmail.com>
2017-05-16 22:15:31 +0200

    Decline and approve project

--- Project Apollo/Scripts/Pages/ProfilePage.js
+++ Project Apollo/Scripts/Pages/ProfilePage.js
@@ -3,4 +3,25 @@
         showForm("signup-form");
     });
     //Deleting current user
+    //Approve post
+    $(".approve-btn").click(function () {
+        var projectContainer = $(this).parent().parent().parent().parent();
+        var selectedProjectId = $(projectContainer).attr("id");
+        $.post("/profile/approveProject", {
+            projectId: selectedProjectId,
+        }, function () {
+            showSnackbar("Project approved");
+            $(projectContainer).remove();
+        });
+    });
+    $(".decline-btn").click(function () {
+        var projectContainer = $(this).parent().parent().parent().parent();
+        var selectedProjectId = $(projectContainer).attr("id");
+        $.post("/profile/declineProject", {
+            projectID: selectedProjectId,
+        }, function () {
+            showSnackbar("Project declined");
+            $(projectContainer).remove();
+        });
+    });
 });
